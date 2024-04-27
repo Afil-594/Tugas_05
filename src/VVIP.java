34d8932a5f2932c394c97cf2be0ed61f8af912e8
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 13:47:06 +0700

    Update VVIP.java

--- src/VVIP.java
+++ src/VVIP.java
@@ -1,4 +1,16 @@
 // Kelas turunan untuk tiket VVIP
 class VVIP extends Tiket {
     // Do your magic here
+    public VVIP() {
+        jenis = "VVIP";
+        harga = 200;
+    }
+  
+    @Override
+    public int getHarga(String tahap) {
+        if (tahap.equalsIgnoreCase("presale")) {
+            return (int) (harga * 0.8);
+        }
+        return harga;
+    }
 }
