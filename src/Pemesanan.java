eaa7525bfe4369dc670567038059e1554a3e48bd
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 17:49:48 +0700

    Update Pemesanan.java

--- src/Pemesanan.java
+++ src/Pemesanan.java
@@ -15,7 +15,7 @@ class Pemesanan {
     public void cetakNota() {
         // Do your magic here
         int totalHarga = tiket.getHarga(tahap) * jumlah;
-        System.out.println("=== Nota Pemesanan ===");
+        System.out.println("\n--- Nota Pemesanan ---");
         System.out.println("Nama: " + nama);
         System.out.println("Tahap Pembelian: " + tahap);
         System.out.println("Jenis Tiket: " + tiket.jenis);
