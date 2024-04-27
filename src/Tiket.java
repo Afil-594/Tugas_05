77bb480fcd6e0cf2c331ea32f0068e0a772caee2
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 13:45:59 +0700

    Update Tiket.java

--- src/Tiket.java
+++ src/Tiket.java
@@ -1,4 +1,8 @@
 // Kelas abstrak untuk semua jenis tiket
 abstract class Tiket {
     //Do your magic here
+    protected String jenis;
+    protected int harga;
+
+    public abstract int getHarga(String tahap);
 }
