251f9c0358d9f3ebc1ea54d63dd34450397dfa81
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 13:46:30 +0700

    Update VIP.java

--- src/VIP.java
+++ src/VIP.java
@@ -1,4 +1,16 @@
 // Kelas turunan untuk tiket VIP
 class VIP extends Tiket {
    // Do your magic here
+   public VIP() {
+      jenis = "VIP";
+      harga = 150;
+  }
+
+  @Override
+  public int getHarga(String tahap) {
+      if (tahap.equalsIgnoreCase("presale")) {
+          return (int) (harga * 0.8);
+      }
+      return harga;
+  }
 }
