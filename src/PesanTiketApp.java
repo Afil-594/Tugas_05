b13aa6ef2d3ef8d3ba9a4d58d5774764ecc0ad7d
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 15:05:18 +0700

    Update PesanTiketApp.java

--- src/PesanTiketApp.java
+++ src/PesanTiketApp.java
@@ -10,7 +10,7 @@ public class PesanTiketApp {
         System.out.print("Masukkan nama Anda: ");
         String nama = scanner.nextLine();
 
-        System.out.println("Pilih tahap pembelian:");
+        System.out.println("\nPilih tahap pembelian:");
         System.out.println("1. Presale");
         System.out.println("2. Reguler");
         System.out.print("Masukkan pilihan Anda (1/2): ");
@@ -19,7 +19,7 @@ public class PesanTiketApp {
 
         if (tahapPilihan == 1) {
             tahap = "Presale";
-            System.out.println("Pilih jenis tiket:");
+            System.out.println("\nPilih jenis tiket:");
             System.out.println("1. VIP");
             System.out.println("2. VVIP");
             System.out.print("Masukkan pilihan Anda (1/2): ");
@@ -37,14 +37,14 @@ public class PesanTiketApp {
                     return;
             }
 
-            System.out.print("Masukkan jumlah tiket: ");
+            System.out.print("\nMasukkan jumlah tiket: ");
             int jumlah = scanner.nextInt();
 
             Pemesanan pemesanan = new Pemesanan(nama, tahap, tiket, jumlah);
             pemesanan.cetakNota();
         } else if (tahapPilihan == 2) {
             tahap = "Reguler";
-            System.out.println("Pilih jenis tiket:");
+            System.out.println("\nPilih jenis tiket:");
             System.out.println("1. Festival");
             System.out.println("2. VIP");
             System.out.println("3. VVIP");
@@ -66,7 +66,7 @@ public class PesanTiketApp {
                     return;
             }
 
-            System.out.print("Masukkan jumlah tiket: ");
+            System.out.print("\nMasukkan jumlah tiket: ");
             int jumlah = scanner.nextInt();
 
             Pemesanan pemesanan = new Pemesanan(nama, tahap, tiket, jumlah);
