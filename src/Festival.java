181484fde754f5c4c89e0d58a059370c89c60899
Afil-594 <149544082+Afil-594@users.noreply.github.com>
2024-04-27 13:49:10 +0700

    Update Festival.java

--- src/Festival.java
+++ src/Festival.java
@@ -1,4 +1,13 @@
 // Kelas turunan untuk tiket Festival
 class Festival extends Tiket {
     // Do your magic here
+    public Festival() {
+        jenis = "Festival";
+        harga = 100;
+    }
+
+    @Override
+    public int getHarga(String tahap) {
+        return harga;
+    }
 }
